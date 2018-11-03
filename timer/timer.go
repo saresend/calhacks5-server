4e51ca4818304417f8a4196334bd545f33bddade
saresend <saresend@usc.edu>
2018-11-03 08:18:41 -0700

    timer skeleton

--- timer/timer.go
+++ timer/timer.go
@@ -1,3 +1,32 @@
 package timer
 
+import (	
+	"time"
+)
+
+var counter int = 15
+
+func Decrement() {
+	timer := time.NewTimer(time.Second)
+	<-timer.C
+	go func() {
+	    counter--
+
+	    if counter == 0 {
+		/* Execute appropriate method here */	
+	    }
+	}()
+}
+
+func AddTime(amt int) {
+	counter += amt
+}
+
+
+
+
+
+
+
+
 
