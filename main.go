cf98eb5d70bed86f5c488c382d9bcc3ffae764d1
Stiven Deleur <deleur@usc.edu>
2018-11-03 19:57:09 -0700

    heroku

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -14,12 +15,17 @@ func main() {
 	state.Init()
 	go Tick()
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/updates", handlers.SocketHandler)
 	r.HandleFunc("/getPrompts", handlers.GetPrompts).Methods("GET")
 	r.HandleFunc("/setPrompts", handlers.SetPrompts).Methods("POST")
 	r.HandleFunc("/vote", handlers.Vote).Methods("POST")
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":"+port, r)
 }
 
 func Tick() {
